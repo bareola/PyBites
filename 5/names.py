2e2eb6bf84ba52c539aff2b6500a7886dd86ff85
bareola <31635437+bareola@users.noreply.github.com>
2022-12-29 17:23:34 +0100

    submission Bite 5 @ codechalleng.es

--- 5/names.py
+++ 5/names.py
@@ -7,13 +7,17 @@ NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
 def dedup_and_title_case_names(names):
     """Should return a list of title cased names,
        each name appears only once"""
-    pass
-
+    sortedNames = []
+    for name in names:
+        if name.title() not in sortedNames:
+            sortedNames.append(name.title())
+    return sortedNames
 
 def sort_by_surname_desc(names):
     """Returns names list sorted desc by surname"""
     names = dedup_and_title_case_names(names)
-    # ...
+    names.sort(key=lambda x: x.split()[-1], reverse=True)
+    return names
 
 
 def shortest_first_name(names):
@@ -21,4 +25,11 @@ def shortest_first_name(names):
        You can assume there is only one shortest name.
     """
     names = dedup_and_title_case_names(names)
-    # ...
+    shortestName = None
+    for name in names:
+        if shortestName == None:
+            shortestName = name.split()[0]
+        elif len(name.split()[0])<len(shortestName):
+            shortestName = name.split()[0]
+    return shortestName
+shortest_first_name(NAMES)
