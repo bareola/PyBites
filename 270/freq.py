7072fbb503807904706951795c6e50af32d42a9a
bareola <31635437+bareola@users.noreply.github.com>
2023-03-11 12:55:24 +0100

    submission Bite 270 @ codechalleng.es

--- 270/freq.py
+++ 270/freq.py
@@ -1,12 +1,17 @@
+from collections import Counter
 def freq_digit(num: int) -> int:
-    list_num = {}
-    mostFreq = []
-    for i in str(num):
-        if i not in list_num:
-            list_num[i]=1
-        else:
-            list_num[i]+=1
-    return int(max(list_num, key=list_num.get))
+    countNum = Counter(str(num))
+    return int(countNum.most_common()[0][0])
+
+# def freq_digit(num: int) -> int:
+#     list_num = {}
+#     mostFreq = []
+#     for i in str(num):
+#         if i not in list_num:
+#             list_num[i]=1
+#         else:
+#             list_num[i]+=1
+#     return int(max(list_num, key=list_num.get))
 
 # def freq_digit(num: int) -> int:
 #     return int(max(str(num), key=str(num).count))
